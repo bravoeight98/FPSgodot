3db5f224394f695d123d4131543525751ddde116
Azfar Shams <azfarshams1998@gmail.com>
2024-01-02 20:05:14 +0600

    Update player.gd

--- FPSgodot/player/player.gd
+++ FPSgodot/player/player.gd
@@ -1,3 +1,11 @@
 extends KinematicBody
 
 const MOUSE_SENSITIVITY = 0.1
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		#Rotate camera verticaly 
+		$CamRoot.rotate_x(deg2rad(event.relative.y * MOUSE_SENSITIVITY * -1))
+		
+		#Rotate camera horizontaly 
+		self.rotate_y(deg2rad(event.relative.x * MOUSE_SENSITIVITY * -1)) 
