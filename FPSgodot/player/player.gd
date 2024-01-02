1d70a4791400fa0660e9dbfb0b44ebf3cfb4c985
Azfar Shams <azfarshams1998@gmail.com>
2024-01-02 20:14:56 +0600

    Update player.gd

--- FPSgodot/player/player.gd
+++ FPSgodot/player/player.gd
@@ -4,6 +4,9 @@ const MOUSE_SENSITIVITY = 0.1
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
+func _process(delta):
+	window_activity()
 
 func _input(event):
 	if event is InputEventMouseMotion:
@@ -12,3 +15,11 @@ func _input(event):
 		
 		#Rotate camera horizontaly 
 		self.rotate_y(deg2rad(event.relative.x * MOUSE_SENSITIVITY * -1)) 
+
+# To show/hide the cursor
+func window_activity():
+	if Input.is_action_just_pressed("ui_cancel"):
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
